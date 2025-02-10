e6cf56222b05d7d3199304fce55da39f0a85f1cb
Francisco <77059089+danielarraiol02@users.noreply.github.com>
2025-02-10 11:47:44 +0000

    Update server.js

--- Ficha1/server.js
+++ Ficha1/server.js
@@ -18,6 +18,7 @@ calculateGrade(12, 12, 12)
 
 // Entrada: 2; Saída: Fevereiro
 
+/*
 function getMonthName(nMes) {
     switch (nMes) {
         case 1:
@@ -53,4 +54,138 @@ function getMonthName(nMes) {
 }
 getMonthName(2)
 getMonthName(3)
-getMonthName(19)
+// getMonthName(19)
+*/
+/*
+function calculate(n1, n2, op){
+    
+    switch(op){
+        case "+":
+            return n1+n2;
+        case '-':
+            return n1-n2;
+        case '*':
+            return n1*n2;
+        case '/':
+            if (n2 == 0){
+                console.log("Não é possível dividir por 0");
+                break;
+            }
+            else
+                return n1/n2;
+        default:
+            console.log("Não é válido");
+            break;
+    }
+}
+console.log("Resultado = " + calculate(2, 2, "+"));
+console.log("Resultado = " + calculate(3, 2, "-"));
+console.log("Resultado = " + calculate(2, 2, "/"));
+console.log("Resultado = " + calculate(4, 2, "*"));
+console.log("Resultado = " + calculate(2, 0, "/"));
+*/
+/*
+function multiplosLowerThan(multiplo, lte){
+
+    console.log("Multiplos de " + multiplo + " menores que " + lte)
+
+    for (i = 1; i < lte; i++) {
+        if (i%multiplo == 0){
+            console.log("" + i);
+        }
+      }
+}
+multiplosLowerThan(5, 20)
+/* ou */
+/*
+ciclo while
+*/
+
+
+// Soma dos primeiros n numeros inteiros
+// 5 -> 1+2+3+4+5
+/*
+function sumUpUntil(n1){
+    var res = 0;
+    for (var i = 1; i <= n1; i++){
+        res += i;
+    }
+    return res;
+}
+
+console.log(sumUpUntil(5))
+
+function fatorialNum(n1){
+    var res = 1;
+    for (var i = 1; i <= n1; i++){
+        res *= i;
+    }
+    return res;
+}
+console.log(fatorialNum(n1));
+*/
+/*
+function maxMinAvgArray(array){
+    var sum = 0;
+    var max = array[0];
+    var min = array[0];
+    var avg = 0;
+
+    for (var i = 0; i < array.length; i++){
+        sum += array[i];
+        if (array[i] > max){
+            max = array[i];
+        }
+        if (array[i] < min){
+            min = array[i];
+        }
+    avg = sum / array.length;
+    }
+    return {sum, max, min, avg}
+}
+var result = maxMinAvgArray([5,3,4,2])
+*/
+function maxArray(array) {
+    var max = array[0];
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+function sumArray(array) {
+    var sum = 0
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+function minArray(array) {
+    var min = array[0];
+
+    for (var i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+function avgArray(array){
+    sum = sumArray(array)
+    return sum/array.length
+}
+
+const array = [2, 3, 5, 10]
+
+console.log(sumArray(array))
+console.log(maxArray(array))
+console.log(minArray(array))
+console.log(avgArray(array))
+//console.log("Soma = " + result.sum);
+//console.log("Máximo = " + result.max);
+//console.log("Mínimo = " + result.min);
+//console.log("Média = " + result.avg);
